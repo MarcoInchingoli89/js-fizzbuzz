90a82903e0367018dddd36a1ea101d782f84b395
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-10 16:59:42 +0200

    Add else

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,18 +13,27 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
 
 /* Istruzioni */
 // Conto i numeri da 1 a 100 e li stampo in console
-for (let number = 1; number < 101; number++) {
-    console.log(number)
+for (let i = 1; i < 101; i++) {
     // Per i multipli di 3 stampo Fizz
-if (number % 3 == 0) {
-    console.log("Fizz")
+    if (i % 3 == 0) {
+        console.log("Fizz");
+    } else {
+        console.log(i);
+    // Per i multipli di 5 stampo Buzz    
+    } else (i % 5 == 0) {
+        console.log("Buzz");
+    } 
+        
+    
 }
-// Per i multipli di 5 stampo Buzz
-else if (number % 5 == 0) {
-    console.log("Buzz")
-}
-// Per i multipli di 3 e 5 stampo FizzBuzz
-else {
-    console.log("FizzBuzz")
-}
-}
+
+    // Per i multipli di 5 stampo Buzz
+/*     else if (i % 5 == 0) {
+        console.log("Buzz")
+    }
+    // Per i multipli di 3 e 5 stampo FizzBuzz
+    else {
+        console.log("FizzBuzz")
+    } */
+   
+
