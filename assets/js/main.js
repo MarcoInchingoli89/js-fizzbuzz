5f32b6a96bd64a2a63f4853abcaa9a1e62a41c98
Marco Inchingoli <evilmarkus@hotmail.it>
2022-10-10 15:15:07 +0200

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/* FizzBuzz */
+
+/* Scrivi un programma che stampi in console i numeri da 1 a 100,
+ma che per i multipli di 3 stampi “Fizz” al posto del numero
+e per i multipli di 5 stampi “Buzz”.
+Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”. */
+
+/* Strumenti che userò */
+// Ciclo for
+// Operatore modulo (%) per i multipli
+// Ciclo if per i vari casi di numeri multipli
+
+
+/* Istruzioni */
+// Conto i numeri da 1 a 100 e li stampo in console
+// Per i multipli di 3 stampo Fizz
+// Per i multipli di 5 stampo Buzz
+// Per i multipli di 3 e 5 stampo FizzBuzz
